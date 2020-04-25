a05fdca2f7f59c63ad55d7e0ddc1551856cfac91
Mahmoud Rayan <mahmoud.rayan@live.com>
2020-04-25 16:42:24 +0200

    Remote Validation

--- Areas/Admin/Models/AuthorViewModel.cs
+++ Areas/Admin/Models/AuthorViewModel.cs
@@ -1,27 +1,31 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyBlog.Areas.Admin.Models
 {
     public class AuthorViewModel
     {
-        
-
-        #region Model Properties
-
+        [Display(Name = "First Name")]
         [Required, MinLength(3), MaxLength(20)]
         public string FirstName {get;set;}
 
+        [Display(Name = "Last Name")]
         public string LastName {get;set;}
 
+        [Display(Name = "Job Title")]
         public string JobTitle {get;set;}
 
+        [Display(Name = "Short Bio")]
         public string ShortBio {get;set;}
 
+        [Display(Name = "Full Bio")]
         public string FullBio {get;set;}
 
         [Required]
+        [PageRemote(PageHandler="CheckPermalinkValidity",
+        ErrorMessage="Permalink already exists",
+        AdditionalFields="__RequestVerificationToken,id",
+        HttpMethod="post")]
         public string Permalink {get;set;}
-
-        #endregion    
     }
 }
--- Areas/Admin/Pages/Authors/Edit.cshtml.cs
+++ Areas/Admin/Pages/Authors/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using MyBlog.Areas.Admin.Models;
 using MyBlog.Data;
 
@@ -34,10 +35,35 @@ namespace MyBlog.Admin.Pages.Authors
             return Page();
         }
 
-        // public async Task<IActionResult> OnPostAsync()
-        // {
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
 
-        // }
+            var entity = await _dbContext.Authors.FindAsync(Id);
+            if (entity == null)
+                return BadRequest();
+
+            entity.FirstName = Author.FirstName;
+            entity.LastName = Author.LastName;
+            entity.JobTitle = Author.JobTitle;
+            entity.FullBio = Author.FullBio;
+            entity.ShortBio = Author.ShortBio;
+            entity.Permalink = Author.Permalink;
+
+            await _dbContext.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+
+        public async Task<JsonResult> OnPostCheckPermalinkValidityAsync(int id, string permalink)
+        {
+            bool permalinkExists = await _dbContext.Authors
+                .AsNoTracking()
+                .AnyAsync(x => x.Id != Id && x.Permalink == Author.Permalink);
+            
+            var isValid = !permalinkExists;
+            return new JsonResult(isValid);
+        }
 
         [BindProperty(SupportsGet=true)]
         public int Id {get;set;}
@@ -46,7 +72,7 @@ namespace MyBlog.Admin.Pages.Authors
         public AuthorViewModel Author
         {
             get;
-            private set;
+            set;
         }
     }
 }
