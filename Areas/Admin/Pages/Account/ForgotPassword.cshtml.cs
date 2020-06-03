6228d142a466d6c527ad4fb6aa5f9772d138e84c
Mahmoud Rayan <mahmoud.rayan@live.com>
2020-06-03 17:59:24 +0200

    Forgot password implementation

--- Areas/Admin/Pages/Account/ForgotPassword.cshtml.cs
+++ Areas/Admin/Pages/Account/ForgotPassword.cshtml.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyBlog.Admin.Services.Email;
 using MyBlog.Data.Models;
 
 namespace MyBlog.Admin.Pages.Account
@@ -13,9 +15,15 @@ namespace MyBlog.Admin.Pages.Account
     public class ForgotPasswordModel : PageModel
     {
         private readonly UserManager<AdminUser> _userManager;
+        private readonly IEmailSender _emailSender;
 
-        public ForgotPasswordModel(UserManager<AdminUser> userManager) 
-            => _userManager = userManager;
+        public ForgotPasswordModel(UserManager<AdminUser> userManager,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _emailSender = emailSender;
+        } 
+            
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -30,10 +38,30 @@ namespace MyBlog.Admin.Pages.Account
             //generate reset password token.
             var passwordResetToken = await _userManager.GeneratePasswordResetTokenAsync(user);
 
-            //send email to the user with the new token.
-            
-            //the new link goes to ResetPassword page.
+            await sendPasswordResetEmail(user.Id, passwordResetToken, user.Email, user.FirstName);
+
             return Page();
+        }
+
+        private Task sendPasswordResetEmail(string userId, string code, string email, string firstName)
+        {
+            //send email to the user with the new token.
+            var passwordResetLink = Url.Page("ResetPassword", 
+                null,
+                new { userId, code },
+                Request.Scheme);
+
+            var emailMessage = new StringBuilder();
+
+            emailMessage.AppendFormat("<p>Dear {0}</p><br><br>", firstName);
+            emailMessage.AppendFormat("<p>You requested to reset your password.</p><br>");
+            emailMessage.AppendFormat("<p>Click on the following link to reset your password:</p><br><br>");
+            emailMessage.AppendFormat("<div style=\"border:1px solid #eee;background-color:#e2e2e2;padding:2px\"><a href=\"{0}\">{0}</a></div>",
+                passwordResetLink);
+
+            //_emailSender.SendAsync()
+            return Task.CompletedTask;
+
         }
 
         [BindProperty]
